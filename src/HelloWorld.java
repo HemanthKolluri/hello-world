490b890661c5830ab0a17045a636a2b82acc9e9b
HemanthKolluri <hemanthkolluri5@gmail.com>
2024-05-24 23:20:39 +0530

    added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,5 @@
 public class HelloWorld {
     public static void main(String [] args) {
-        for (int i =0;i<5;i++){
-            System.out.println("Hello world");
-        }
-
+        System.out.println("Hello world");
     }
 }
