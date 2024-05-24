49f78bf4d795d6939897a758e89b029b2e3385f3
HemanthKolluri <hemanthkolluri5@gmail.com>
2024-05-24 23:50:48 +0530

    learned till datatypes

---
+++ src/dataTypes.java
@@ -0,0 +1,18 @@
+public class dataTypes {
+    public static void main(String[] args) {
+        int a=7;
+        int b=3;
+        int x=3,y=5;
+        System.out.println(a+b);
+        System.out.println(x+y);
+        System.out.println("hello"+x+y);
+        System.out.println("hello "+(x+y));
+        double q=3.4;
+        double p=1.5;
+        System.out.println(q+p);
+        /* hello comments */
+        //is this a comment, yess it is
+        double t=a*b-x*y;
+        System.out.println(t);
+    }
+}
