18188c6a1ba98fab3d84d4d68af26ccaf9eb6613
HemanthKolluri <hemanthkolluri5@gmail.com>
2024-05-25 20:42:30 +0530

    string and i/0

---
+++ src/StringsEX.java
@@ -0,0 +1,9 @@
+public class StringsEX {
+    public static void main(String[] args) {
+        String a="Hello";
+        System.out.println(a+" world");
+        String b=" W0rlds";
+        System.out.println(a+b);
+        System.out.println("My name is \t Hemanth, \nidgaf  ");
+    }
+}
---
+++ src/input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter value : ");
+        int a= sc.nextInt();
+        System.out.println("enter value : ");
+        double b= sc.nextFloat();
+        System.out.println(a + b);
+    }
+}
