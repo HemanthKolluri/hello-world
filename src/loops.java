565a90135b6ff77a88913f0002089bdeb91bec65
HemanthKolluri <hemanthkolluri5@gmail.com>
2024-05-25 21:05:52 +0530

    while loop

---
+++ src/loops.java
@@ -0,0 +1,13 @@
+public class loops {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<5){
+            System.out.println("hello World "+i);
+            i++;
+        }
+        i=0;
+        while (i<5){
+            System.out.println("hello World "+i++);
+        }
+    }
+}
