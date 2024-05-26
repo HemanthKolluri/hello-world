42b018821ba5a16fb26e44110ace156f2f448e6c
HemanthKolluri <hemanthkolluri5@gmail.com>
2024-05-26 12:27:04 +0530

    added string cont

---
+++ src/stringCont.java
@@ -0,0 +1,15 @@
+public class stringCont {
+    public static void main(String[] args) {
+        String a="hello World";
+        System.out.println(a.length());
+        System.out.println(a.toLowerCase());
+        System.out.println(a.substring(3,5));
+        System.out.println(a.charAt(1));
+        System.out.println(a.substring(3,5));
+        System.out.println(a.indexOf("h"));
+        System.out.println(a.equals("Hello W0rld"));
+        int x=Integer.parseInt("7");
+        System.out.println(x);
+
+    }
+}
