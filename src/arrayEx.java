24029559ba7f98ad467ed20b66e6c5ea3a15f674
HemanthKolluri <hemanthkolluri5@gmail.com>
2024-06-01 19:30:11 +0530

    classes and Object

---
+++ src/arrayEx.java
@@ -0,0 +1,5 @@
+public class arrayEx {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/classesAndObj.java
@@ -0,0 +1,11 @@
+
+public class Person{
+    String name;
+}
+public class classesAndObj {
+    public static void main(String[] args) {
+        Person p1 =new Person();
+        p1.name="Hemanth";
+        System.out.println(p1.name);
+    }
+}
